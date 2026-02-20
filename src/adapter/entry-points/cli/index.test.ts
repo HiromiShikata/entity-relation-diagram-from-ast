9c3dddf21fb773158ae7da64df40de8e3d89b88f
HiromiShikata <shikata.hiromi@gmail.com>
2026-02-20 19:32:36 +0000

    chore(src): add ts-node as devDependency for hermetic CLI tests
    - Add ts-node as devDependency so CLI tests don't rely on npx downloading it at runtime
    - Update CLI integration tests to use local ./node_modules/.bin/ts-node instead of npx ts-node

--- src/adapter/entry-points/cli/index.test.ts
+++ src/adapter/entry-points/cli/index.test.ts
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 describe('commander program', () => {
   it('should output contents', async () => {
     const output = execSync(
-      'npx ts-node ./src/adapter/entry-points/cli/index.ts ./node_modules/ast-to-entity-definitions/testdata/src/domain/entities',
+      'npm exec -- ts-node ./src/adapter/entry-points/cli/index.ts ./node_modules/ast-to-entity-definitions/testdata/src/domain/entities',
     ).toString();
     const expectedContent = `erDiagram
   Administrator {
@@ -67,7 +67,7 @@ describe('commander program', () => {
 
   it('should output help', () => {
     const output = execSync(
-      'npx ts-node ./src/adapter/entry-points/cli/index.ts --help',
+      'npm exec -- ts-node ./src/adapter/entry-points/cli/index.ts --help',
       {
         encoding: 'utf8',
         maxBuffer: 1024 * 1000,
